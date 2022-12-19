1e1a6d0cce7a4861b3be60d17d4c92e22f27c20f
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2022-12-19 12:00:16 +0800

    fix options index for windows

--- copilot/copilot.py
+++ copilot/copilot.py
@@ -8,32 +8,47 @@ import os
 from urllib.parse import quote
 import platform
 
-if platform.system().lower().startswith("lin") or platform.system().lower().startswith("dar"):
+if platform.system().lower().startswith("lin") or platform.system().lower().startswith(
+    "dar"
+):
     from simple_term_menu import TerminalMenu
-    from copilot import history
 elif platform.system().lower().startswith("win"):
     import inquirer
 
+from copilot import history
 
 
-if platform.system().lower().startswith("lin") or platform.system().lower().startswith("dar"):
+if platform.system().lower().startswith("lin") or platform.system().lower().startswith(
+    "dar"
+):
     from simple_term_menu import TerminalMenu
 elif platform.system().lower().startswith("win"):
     import inquirer
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='copilot', description='Terminal Copilot')
-    parser.add_argument('command', type=str, nargs='+',
-                        help='Describe the command you are looking for.')
-    parser.add_argument('-a', '--with-aliases', action='store_true',
-                        help='Include aliases in the prompt. Note: This feature may potentially send sensitive information to OpenAI.')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='increase output verbosity')
-    parser.add_argument('-g', '--git', action='store_true',
-                        help='Include git info if available')
-    parser.add_argument('-hist', '--history', action='store_true',
-                        help='Include terminal history in the prompt. Note: This feature may potentially send sensitive information to OpenAI and increase the number of tokens used.')
+    parser = argparse.ArgumentParser(prog="copilot", description="Terminal Copilot")
+    parser.add_argument(
+        "command", type=str, nargs="+", help="Describe the command you are looking for."
+    )
+    parser.add_argument(
+        "-a",
+        "--with-aliases",
+        action="store_true",
+        help="Include aliases in the prompt. Note: This feature may potentially send sensitive information to OpenAI.",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="increase output verbosity"
+    )
+    parser.add_argument(
+        "-g", "--git", action="store_true", help="Include git info if available"
+    )
+    parser.add_argument(
+        "-hist",
+        "--history",
+        action="store_true",
+        help="Include terminal history in the prompt. Note: This feature may potentially send sensitive information to OpenAI and increase the number of tokens used.",
+    )
 
     args = parser.parse_args()
 
@@ -50,7 +65,9 @@ def main():
             environs += f"{key}={os.environ[key]}\n"
 
     operating_system = platform.system()
-    if operating_system.lower().startswith("lin") or operating_system.lower().startswith("dar"):
+    if operating_system.lower().startswith(
+        "lin"
+    ) or operating_system.lower().startswith("dar"):
         shell = os.environ.get("SHELL")
     elif operating_system.lower().startswith("win"):
         shell = "cmd"
@@ -60,7 +77,7 @@ def main():
 
     # list the files in the current directory
     directory_list = os.listdir()
-    
+
     prompt = f"""
 You are an AI Terminal Copilot. Your job is to help users find the right terminal command in a {shell} on {operating_system}.
 
@@ -76,7 +93,11 @@ The user has several environment variables set, some of which are:
 {environs}
 {git_info() if args.git else ""}
 """
-    if args.with_aliases and operating_system.lower().startswith("lin") or operating_system.lower().startswith("dar"):
+    if (
+        args.with_aliases
+        and operating_system.lower().startswith("lin")
+        or operating_system.lower().startswith("dar")
+    ):
         prompt += f"""
 The user has the following aliases set:
 {subprocess.run(["alias"], capture_output=True, shell=True).stdout.decode("utf-8")}
@@ -105,23 +126,26 @@ The command the user is looking for is:
 
 def show_command_options(prompt, cmd):
     operating_system = platform.system()
-    
+
     print(f"\033[94m> {cmd}\033[0m")
     options = ["execute", "copy", "explainshell", "show more options"]
-    
-    if operating_system.lower().startswith("lin") or operating_system.lower().startswith("dar"):
+
+    if operating_system.lower().startswith(
+        "lin"
+    ) or operating_system.lower().startswith("dar"):
         terminal_menu = TerminalMenu(options)
         menu_entry_index = terminal_menu.show()
     elif operating_system.lower().startswith("win"):
         questions = [
-            inquirer.List('menu_entry_index',
-                        message="What do you want to do?",
-                        choices=options,
-                    ),
+            inquirer.List(
+                "menu_entry_index",
+                message="What do you want to do?",
+                choices=options,
+            ),
         ]
         answers = inquirer.prompt(questions)
-        menu_entry_index = answers["menu_entry_index"]
-    
+        menu_entry_index = options.index(answers["menu_entry_index"])
+
     if menu_entry_index == 0:
         execute(cmd)
     elif menu_entry_index == 1:
@@ -142,24 +166,27 @@ def execute(cmd):
 
 def show_more_cmd_options(prompt):
     operating_system = platform.system()
-    
+
     cmds = request_cmds(prompt, n=5)
     print("Here are more options:")
     options = [repr(cmd) for cmd in cmds]
-    
-    if operating_system.lower().startswith("lin") or operating_system.lower().startswith("dar"):
+
+    if operating_system.lower().startswith(
+        "lin"
+    ) or operating_system.lower().startswith("dar"):
         cmd_terminal_menu = TerminalMenu(options)
         cmd_menu_entry_index = cmd_terminal_menu.show()
     elif operating_system.lower().startswith("win"):
         questions = [
-            inquirer.List('cmd_menu_entry_index',
-                        message="Which command do you want to execute?",
-                        choices=options,
-                    ),
+            inquirer.List(
+                "cmd_menu_entry_index",
+                message="Which command do you want to execute?",
+                choices=options,
+            ),
         ]
         answers = inquirer.prompt(questions)
-        cmd_menu_entry_index = answers["cmd_menu_entry_index"]    
-    
+        cmd_menu_entry_index = options.index(answers["cmd_menu_entry_index"])
+
     if cmd_menu_entry_index is not None:
         show_command_options(prompt, cmds[cmd_menu_entry_index])
 
@@ -188,7 +215,9 @@ def strip_all_whitespaces_from(choices):
 
 
 def git_info():
-    git_installed = subprocess.run(["which", "git"], capture_output=True).returncode == 0
+    git_installed = (
+        subprocess.run(["which", "git"], capture_output=True).returncode == 0
+    )
     if os.path.exists(".git") and git_installed:
         return f"""
 User is in a git repo.
