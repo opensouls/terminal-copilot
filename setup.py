4ca03c2753f88a26255182d54810b76c9304c224
Joel <joelkronander@gmail.com>
2022-12-23 17:40:40 -0800

    bump verson

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = readme
 
 setup(
         name ='terminal-copilot',
-        version ='1.2.1',
+        version ='1.2.2',
         author ='Methexis',
         author_email ='joelkronander@gmail.com',
         url ='https://github.com/Methexis-Inc/terminal-copilot',
